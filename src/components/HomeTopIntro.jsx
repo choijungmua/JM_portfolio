a20b765bdcc1b7b923010a169ea2546bc5282714
Jungmu <wjdan022@naver.com>
2024-04-13 19:59:44 +0900

    Chore : Styled Components 추가 Feat : 공 움직이기

--- src/components/HomeTopIntro.jsx
+++ src/components/HomeTopIntro.jsx
@@ -1,25 +1,9 @@
-import Next from "../assets/next.png";
-import Javascript from "../assets/javascript.png";
-import React from "../assets/React.png";
-import Recoil from "../assets/Recoil.png";
-import Sass from "../assets/sass.png";
-import Styled from "../assets/styledcomponents.png";
-import Tailwind from "../assets/tailwind.png";
-import HTML5 from "../assets/HTML.png";
-import CSS3 from "../assets/CSS.png";
-
+import BounceBall from "./BounceBall";
 function HomeTopIntro() {
   return (
     <div className="bg-blue-100">
-      <img src={Next} alt="next.js" className="w-[25px]" />
-      <img src={Javascript} alt="Javascript.js" className="w-[25px]" />
-      <img src={Recoil} alt="Javascript.js" className="w-[25px]" />
-      <img src={Sass} alt="Javascript.js" className="w-[25px]" />
-      <img src={CSS3} alt="Javascript.js" className="w-[25px]" />
-      <img src={HTML5} alt="Javascript.js" className="w-[25px]" />
-      <img src={Tailwind} alt="Javascript.js" className="w-[25px]" />
-      <img src={React} alt="Javascript.js" className="w-[25px]" />
-      <img src={Styled} alt="Javascript.js" className="w-[25px]" />
+      <BounceBall />
+
       <div className=" bg-white text-black h-screen flex justify-center flex-col items-center">
         <h1 className="text-3xl mb-3 ">취업 성공 평균 확률</h1>
         <div className="border-black border-2 opacity-20 w-[50px] mb-5" />
