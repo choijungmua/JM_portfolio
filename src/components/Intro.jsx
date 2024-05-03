6e6d3aef3d37ff0abd1a1050a4c1013d6dcd7640
Jungmu <wjdan022@naver.com>
2024-05-04 00:30:55 +0900

    Add The Rounds

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,17 +13,36 @@ function SubAbout() {
       y: 150,
       scrollTrigger: {
         trigger: "#textBox22",
-        markers: true,
         scrub: true,
         start: "center center",
         end: "30% top",
       },
       ease: "none", // easing 효과 없음
     });
+    gsap.to(".gd", {
+      x: 3000,
+      scale: 20,
+      scrollTrigger: {
+        trigger: "#textBox22",
+        markers: true,
+        scrub: true,
+        start: "center center",
+        end: "bottom top",
+      },
+      ease: "none", // easing 효과 없음
+    });
   }, []);
 
   return (
-    <div id="textBox22" className="w-[100vw] h-full relative">
+    <div id="textBox22" className="w-[100vw] h-full relative overflow-hidden">
+      <div className="flex h-full flex-col justify-around gap-10 absolute">
+        <div className="gd w-[7px] h-[7px] bg-red-400 rounded-full"></div>
+        <div className="gd w-[10px] h-[10px] bg-sky-400 rounded-full"></div>
+        <div className="gd w-[2px] h-[2px] bg-blue-400 rounded-full"></div>
+        <div className="gd w-[16px] h-[16px] bg-green-400 rounded-full"></div>
+        <div className="gd w-[8px] h-[8px] bg-sky-400 rounded-full"></div>
+        <div className="gd w-[10px] h-[10px] bg-purple-400 rounded-full"></div>
+      </div>
       <div className="flex w-full h-full items-center">
         <p className="flex overflow-hidden justify-center font-nanum-square-neo-Bold text-6xl whitespace-nowrap">
           <p id="Text" className="">
