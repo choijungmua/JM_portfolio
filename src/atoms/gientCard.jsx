e3621e6bd93423619f780339c84cef2700dca7d0
Jungmu <wjdan022@naver.com>
2024-05-10 09:26:25 +0900

    Feat : Recoil

--- src/atoms/gientCard.jsx
+++ src/atoms/gientCard.jsx
@@ -6,5 +6,21 @@ const gientCard = atom({
   key: "gientCard", // unique ID (with respect to other atoms/selectors)
   default: false, // default value (aka initial value)
 });
+const cardMainText = atom({
+  key: "cardMainText", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+const cardIcon = atom({
+  key: "cardIcon", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+const cardExplain = atom({
+  key: "cardExplain", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+const cardSubText = atom({
+  key: "cardMainText", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
 
 export default gientCard;
